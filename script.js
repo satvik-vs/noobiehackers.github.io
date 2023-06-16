2d307850c5ea372b24d28c4da47ab471620a2121
satvik vemulapalli <111590060+satvik-vs@users.noreply.github.com>
2023-06-16 20:24:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Smooth scrolling effect for navigation links
+document.addEventListener('DOMContentLoaded', function() {
+  const links = document.querySelectorAll('nav a');
+
+  for (const link of links) {
+    link.addEventListener('click', clickHandler);
+  }
+
+  function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+      top: offsetTop,
+      behavior: 'smooth'
+    });
+  }
+});
